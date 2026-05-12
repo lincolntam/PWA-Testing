fe72bb79946036b2e2751cebee0543b140287623
lincolntam <lincolntam56@hotmail.com>
2026-05-12 13:16:46 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,32 @@
-const CACHE = "ltravellog-v0.55.0";
+const CACHE_NAME = "mypwa-v1";
 const ASSETS = [
+  "./",
   "./index.html",
-  "./route.css",
-  "./route.js",
+  "./styles.css",
   "./manifest.json",
-  "./icons/icon-192x192.png"
+  "./assets/bg/bg1.jpg",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png"
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request))
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
